786a820a8261067738cc3031b76312bf836cb2d9
Aaron Tian <Aaron2Ti@gmail.com>
2014-04-15 17:37:09 +0800

    some renaming

--- workers/restart_apache.rb
+++ workers/restart_apache.rb
@@ -4,11 +4,11 @@ EM.run {
   connection = AMQP.connect host: '127.0.0.1'
   channel    = AMQP::Channel.new(connection)
 
-  q1 = channel.queue('com.rakuten.chef.direct.apaches', durable: true)
+  queue = channel.queue('com.rakuten.chef.direct.apaches', durable: true)
 
-  q1.bind('com.rakuten.chef.direct', routing_key: 'com.rakuten.chef.restart_apache')
+  queue.bind('com.rakuten.chef.direct', routing_key: 'com.rakuten.chef.restart_apache')
 
-  q1.subscribe do |payload|
-    puts "Queue #{q1.name} received: #{payload}"
+  queue.subscribe do |payload|
+    puts "Queue #{queue.name} received: #{payload}"
   end
 }
